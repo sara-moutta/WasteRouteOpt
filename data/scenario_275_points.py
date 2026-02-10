6881f215349e0524e8e119936c3edb3e672d6d27
Sara Meira Moutta <smmoutta@uesc.br>
2026-02-10 11:31:52 -0300

    Delete data directory

--- data/scenario_275_points.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("277_stop_regiao1Teste.csv", delimiter=";")
-
-coords = df[['xFeet', 'yFeet']].to_numpy()
-demands = df['demand'].astype(int).to_numpy()
-
-vehicle_capacity = 200
